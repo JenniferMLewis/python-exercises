e8087084f2c5420dbaa06efe17b2653e3cec2ab6
JenniferMLewis <lewis.jennifer.marie.15@gmail.com>
2022-07-12 09:18:03 -0500

    Hello, World! In many versions

---
+++ python_introduction_exercises.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+
+Welcome = "Hello, World!"
+print(Welcome)
+
+firstword= "Hello, "
+secondword = "World!"
+print(firstword + secondword)
+
+def Hello():
+    print("Hello, World!")
+Hello()
